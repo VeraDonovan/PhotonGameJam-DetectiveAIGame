b7c187f60518b841bb9017d9f20faffb3642f8ab
liumo-pie <2951985106@qq.com>
2026-04-11 18:55:12 +0800

    按钮改成点击f对话

--- Assets/Scripts/Npc/NPCInteraction.cs
+++ Assets/Scripts/Npc/NPCInteraction.cs
@@ -3,42 +3,40 @@ using UnityEngine.UI;
 
 public class NPCInteraction : MonoBehaviour {
     public Transform player;              // 玩家对象
-    public GameObject interactionPanel;   // 包含两个按钮的UI面板
-    public Button talkButton;
-    public Button otherButton;
-
+    public GameObject promptText;         // 提示UI，比如一个Text或Image
     public float interactDistance = 3f;   // 交互距离
 
     private NPCAssembler assembler;
 
     void Awake() {
         assembler = GetComponent<NPCAssembler>();
-        interactionPanel.SetActive(false);
-
-        // 按钮绑定事件
-        talkButton.onClick.AddListener(OnTalkClicked);
-        otherButton.onClick.AddListener(OnOtherClicked);
+        promptText.SetActive(false); // 默认隐藏提示
     }
 
     void Update() {
         float distance = Vector2.Distance(transform.position, player.position);
+
         if (distance <= interactDistance) {
-            interactionPanel.SetActive(true);
-            Debug.Log("遇到的npc的姓名是: " + assembler.dialogueConfig.displayName);
-            // 让面板跟随 NPC 出现（在 NPC 上方）
-            interactionPanel.transform.position = Camera.main.WorldToScreenPoint(transform.position + Vector3.up * 1.5f);
+            // 显示提示
+            promptText.SetActive(true);
+            promptText.transform.position = Camera.main.WorldToScreenPoint(transform.position + Vector3.up * 1.5f);
+
+            // 检测玩家是否按下 F 键
+            if (Input.GetKeyDown(KeyCode.F)) {
+                OnInteract();
+            }
         } else {
-            interactionPanel.SetActive(false);
+            promptText.SetActive(false);
         }
     }
 
-    void OnTalkClicked() {
-        Debug.Log("👉 点击了对话按钮");
-        Debug.Log("📊 NPC数据: " + assembler.dialogueConfig.displayName);
-        DialogueManager.Instance.ShowDialogue(assembler.dialogueConfig.initialStatement);
-    }
+    void OnInteract() {
+        Debug.Log("👉 玩家按下 F 与 NPC 交互: " + assembler.dialogueConfig.displayName);
 
-    void OnOtherClicked() {
-        Debug.Log("👉 点击了另一个功能");
+        // 这里可以选择不同功能，比如进入对话
+        DialogueManager.Instance.ShowDialogue(assembler.dialogueConfig.initialStatement);
+
+        // 或者执行其他功能
+        // Debug.Log("执行其他功能");
     }
 }
